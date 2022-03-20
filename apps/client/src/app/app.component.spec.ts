b35b5104bd4c98e8f7a2c8ac917501a859516739
joshuabrink1 <u19185678@tuks.co.za>
2022-03-20 15:20:35 +0200

    removed sha derive

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -28,7 +28,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcomlient'
+      'Welcient'
     );
   });
 });
